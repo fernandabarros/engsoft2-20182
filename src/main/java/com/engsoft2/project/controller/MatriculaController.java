0b0b5db61b1da702d55067dbd449944da2eab4cb
Mateus Barbosa <mateusbarbosa@infojr.com.br>
2018-11-25 17:37:26 -0300

    correções

--- src/main/java/com/engsoft2/project/controller/MatriculaController.java
+++ src/main/java/com/engsoft2/project/controller/MatriculaController.java
@@ -1,5 +1,16 @@
 package com.engsoft2.project.controller;
 
-public class MatriculaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class MatriculaController {
+	
+	@GetMapping("/aluno/transferencia")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Fulano") String name, Model model) {
+        model.addAttribute("name", name);
+        return "transfer";
+    }
 }
