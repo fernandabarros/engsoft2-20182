7855dfa5b6472e123fac715eb2a74f6c53b0d46f
Mateus Barbosa <mateusbarbosa@infojr.com.br>
2018-11-25 20:25:55 -0300

    adiciona requisições ajax para pegar bairros e escolas

--- src/main/java/com/engsoft2/project/controller/MatriculaController.java
+++ src/main/java/com/engsoft2/project/controller/MatriculaController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MatriculaController {
@@ -16,12 +18,63 @@ public class MatriculaController {
 		cities.add("Salvador");
 		cities.add("Feira de Santana");
 		cities.add("Camaçari");
-		cities.add("Porto Seguro");
-		cities.add("Ilhéus");
-		cities.add("Paulo Afonso");
 		
 		model.addAttribute("cities", cities);
 		
         return "transfer";
+    }
+	
+	@ResponseBody
+	@GetMapping("/getNeighborhoods")
+    public List<String> getNeighborhoods(@RequestParam(name="city", required=true) String city) {
+		List<String> neighborhoods = new ArrayList<String>();
+		
+		switch (city) {		
+			case "Salvador":
+				neighborhoods.add("Barbalho");
+				neighborhoods.add("Barra");
+				neighborhoods.add("Ondina");
+				break;
+			case "Feira de Santana":
+				neighborhoods.add("Lagoa Grande");
+				neighborhoods.add("Pedra Ferrada");
+				neighborhoods.add("Campo Limpo");
+				break;
+			case "Camaçari":
+				neighborhoods.add("Parque Satélite");
+				neighborhoods.add("Ponto Certo");
+				neighborhoods.add("Gravatá");
+				break;
+			default:
+				
+		}
+		
+        return neighborhoods;
+    }
+	
+	@ResponseBody
+	@GetMapping("/getSchools")
+    public List<String> getSchools(@RequestParam(name="neighborhood", required=true) String neighborhood) {
+		List<String> schools = new ArrayList<String>();
+		
+		switch (neighborhood) {		
+			case "Barbalho":
+				schools.add("Uma escola");
+				schools.add("Outra escola");
+				break;
+			case "Barra":
+				schools.add("Uma escola");
+				schools.add("Outra escola");
+				break;
+			case "Ondina":
+				schools.add("Uma escola");
+				schools.add("Outra escola");
+				break;
+			default:
+				schools.add("Uma escola");
+				schools.add("Outra escola");
+		}
+		
+        return schools;
     }
 }
